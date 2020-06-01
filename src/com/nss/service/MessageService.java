f464e83cdfcece22aa8c3f91ec083313a761851f
Rasika Ranepura <rcranepura@gmail.com>
2020-06-01 12:33:04 +0530

    Pagination and Filtering

--- src/com/nss/service/MessageService.java
+++ src/com/nss/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.nss.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,26 @@ public class MessageService {
 
 	public List<Message> getAllMessages(){
 		return new ArrayList<Message>(messages.values());
+	}
+	
+	public List<Message> getAllMessagesForYear(int year){
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message: messages.values()) {
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size){
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if(start + size > list.size()) {
+			return new ArrayList<Message>();
+		}
+		return list.subList(start, start + size);
 	}
 	
 	public Message getMessage(long id) {
