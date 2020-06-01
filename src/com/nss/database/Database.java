d5ba5ad6280635b542b93af0165019e6ce477c55
Rasika Ranepura <rcranepura@gmail.com>
2020-06-01 11:37:23 +0530

    Implementing profile resources

--- src/com/nss/database/Database.java
+++ src/com/nss/database/Database.java
@@ -9,13 +9,13 @@ import com.nss.model.Profile;
 public class Database {
 	
 	private static Map<Long,Message> messages = new HashMap<>();
-	private static Map<Long,Profile> profiles = new HashMap<>();
+	private static Map<String,Profile> profiles = new HashMap<>();
 	
 	public static Map<Long, Message> getMessages(){
 		return messages;
 	}
 	
-	public static Map<Long, Profile> getProfiles(){
+	public static Map<String, Profile> getProfiles(){
 		return profiles;
 	}
 
