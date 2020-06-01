0cecbd3d0f4d9f99792e1e548ab1b5bfd2b48b06
Rasika Ranepura <rcranepura@gmail.com>
2020-06-01 12:55:30 +0530

    The Param Annotation

---
+++ src/com/nss/InjectDemoResource.java
@@ -0,0 +1,24 @@
+package com.nss;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+	
+	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+			                                @HeaderParam("param") String headerParam,
+			                                @CookieParam("param") String cookieParam) {
+		
+		
+		return "Matrix Param : " + matrixParam + "Header Param : " + headerParam + "Cookie Param : " + cookieParam;
+	}
+
+}
